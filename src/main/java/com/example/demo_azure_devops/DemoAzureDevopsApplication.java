24b18e46fb7595dde29b447f3ed0ebe69bdf46cc
Anshika Mishra <Anshika.Mishra@walmartlabs.com>
2022-04-15 02:38:50 +0530

    first commit

---
+++ src/main/java/com/example/demo_azure_devops/DemoAzureDevopsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_azure_devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAzureDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAzureDevopsApplication.class, args);
+	}
+
+}
